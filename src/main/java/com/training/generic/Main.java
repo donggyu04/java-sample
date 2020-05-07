ee201290db9940a92dd6376b2200212b0359021f
donggyu.lee <donggyu.lee@nts-corp.com>
2020-05-07 09:24:51 +0900

    Covariant and invariant

--- src/main/java/com/training/generic/Main.java
+++ src/main/java/com/training/generic/Main.java
@@ -1,7 +1,16 @@
 package com.training.generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // 배열 공변
+        Object[] objectArray = new String[5];
+        objectArray[0] = "String";
+        //objectArray[1] = 10;
+
+        // generic 불공변
+        // List<Object> objectList = new ArrayList<String>();
     }
 }
